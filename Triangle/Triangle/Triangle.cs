1c5cea0a19a812f55c8e022cd390130aa1d9e95c
Smirnova <e.smirnova@OSG.lan>
2017-07-19 11:34:13 +0300

    Triangle

---
+++ Triangle/Triangle/Triangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        public double sideA { get; set; }
+        public double sideB { get; set; }
+        public double sideC { get; set; }
+        private double p { get; set; }
+        public double S { get; private set; }
+
+        private void Semiperimeter(double a, double b, double c)
+        {
+            p = (a + b + c) / 2;
+        }
+
+        public void TriangleArea()
+        {
+            Semiperimeter(sideA, sideB, sideC);
+            S = Math.Sqrt((p * (p - sideA) * (p - sideB) * (p - sideC)));
+        }
+    }
+}
